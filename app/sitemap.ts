61a1d5e5ac13ece2c74e2878293ef0976f9f8dfd
hirenmasaliya <112078959+hirenmasaliya@users.noreply.github.com>
2026-04-07 18:34:40 +0530

    sitemap improve

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://clothiva-amber.vercel.app'
+  const baseUrl = 'https://clothivaelite.vercel.app'
 
   // Core Marketing Routes
   const routes = [
